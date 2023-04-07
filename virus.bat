b5388f30f19e5a705438a36adf76bb1e7e19f6ac
VIRUSCREATOR404pl <130188103+VIRUSCREATOR404pl@users.noreply.github.com>
2023-04-07 17:12:13 -0500

    Add files via upload

--- virus.bat
+++ virus.bat
@@ -1,6 +1,7 @@
 @echo off
 color a2
 echo, welcome to my malware data base in this area you will be able to select from a variety of malwares I have made(my og acount got supended) 1-flouriantimonicacid.exe 2-hgfksbgfhnj.exe more updates coming soon.( 1 and 2 are fake gdi programs):)
+set /p %input%=
 if /i %input%==1 goto 1
 if /i %input%==2 goto 2
 if /i %input%==3 goto 3
